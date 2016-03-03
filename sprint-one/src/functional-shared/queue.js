5f198bfd4465e355fb9e460401c27ac36aa377b7
conniedaisy <conniedcheung@gmail.com>
2016-03-02 17:17:39 -0800

    Complete up to reports a size of 2 after adding two items.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,18 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.index = 0;
+
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
-
+queueMethods.size = function() {
+  return this.index;
+};
+queueMethods.enqueue = function(value) {
+  this.index++;
+};
