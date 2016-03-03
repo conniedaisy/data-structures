9fb7b6f3f8d214e39852dd0cd0234b294e9d641b
conniedaisy <conniedcheung@gmail.com>
2016-03-03 12:59:53 -0800

    Complete up to should return the value of the former head when removeHead is called.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,18 +1,45 @@
 var LinkedList = function() {
   var list = {};
+  var someInstance = {};
   list.head = null;
   list.tail = null;
-
+  var index = 0;
+  var headNum = 0;
+  // list.where = 'list.tail';
+  // list.where = list.where.concat('.next');
+  // JSON.parse(list.where);
   list.addToTail = function(value) {
+    //no null yet
+    list[index] = Node(value); // list['0'] = {value:4, next:null}
 
-    this.tail = Node(value); //not getting {'4':4}, getting {'value':4} 
-    // console.log(result);
-    // this.tail = result['value']; //list.tail = value, instead of null
-    // console.log(this.tail);
+    list.tail = list[index];
+    if (list.head === null) {
+      list.head = list.tail;
+    }
+    index++;
 
+    // // var objX = Node(value);
+    // if (list === {}) {
+    //   list = Node(value); //{'value':4, 'next':null}
+    // } else {
+    //   list.next = Node(value); // 'next': {'value':5, 'next':null}
+    // }
+
+    // if (list.tail === null) {
+    //   list.tail = list; // list.tail = {'value':4, 'next':null} 
+    // } else {
+    //   list.tail = list.next;
+    // }
+    // if (list.head === null) {
+    //   list.head = list.tail;
+    // }
   };
 
   list.removeHead = function() {
+    var lastHeadValue = this[headNum].value;
+    headNum++;
+    list.head = this[headNum];
+    return lastHeadValue;
   };
 
   list.contains = function(target) {
@@ -34,8 +61,10 @@ var Node = function(value) {
 //{something: 4} --> this.tail = 4
 //addToTail(5)
 //{something:4, something: 5} --> this.tail = 5
-
-
+//{value:4, next: {value:5, next: {value: 6, next: null} } }
+//{value:4, next: } -> {value:5} -> {}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// list.tail.value = 4;
+// list.tail.value.value = 5
