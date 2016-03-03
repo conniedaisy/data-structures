77fe9a4a2a6da6fd9af2b5b05c0a37c1a1984d9f
conniedaisy <conniedcheung@gmail.com>
2016-03-02 16:33:09 -0800

    Complete up to reports a size of zero for a new stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,57 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.index = 0;
+
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  // var size = 0;
+  return this.index; //someInstance.size
+};
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var Stack = function() {
+//   var someInstance = {};
+
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+//   var index = 0;
+//   // Implement the methods below
+//   someInstance.push = function(value) {
+//     storage[index] = value;
+//     index++;
+//   };
+
+//   someInstance.pop = function() {
+//     index--;
+//     return storage[index];
+//   };
+
+//   someInstance.size = function() {
+//     if (index < 0) {
+//       index = 0;
+//     }
+//     return index;
+//   };
+
+//   return someInstance;
+// };
