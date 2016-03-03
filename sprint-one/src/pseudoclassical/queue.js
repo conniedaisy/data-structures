995606d749d379889e3b49baaacf32e288f6701a
conniedaisy <conniedcheung@gmail.com>
2016-03-02 20:54:02 -0800

    Complete up to reports a size of zero for a new queue.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,12 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.index = 0;
 };
 
+Queue.prototype.size = function() {
+  return this.index;
+};
+
+var someInstance = new Queue();
 
