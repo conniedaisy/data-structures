feda3b6c7439b9bf6039c96eed8f4c620081fc18
conniedaisy <conniedcheung@gmail.com>
2016-03-02 20:27:30 -0800

    Complete up to reports a size of zero for a new queue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.index = 0;
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
-
+queueMethods.size = function() {
+  return this.index;
+};
